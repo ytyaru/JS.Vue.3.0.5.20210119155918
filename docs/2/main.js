978d9999bcf9d5f09cdb6febbf8324fcdd6d0a67
ytyaru <yttry0@gmail.com>
2021-01-19 16:28:41 +0900

    v-on

---
+++ docs/2/main.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', (event) => {
+    const EventHandling = {
+      data() {
+        return {
+          message: 'Hello Vue.js!'
+        }
+      },
+      methods: {
+        reverseMessage() {
+          this.message = this.message
+            .split('')
+            .reverse()
+            .join('')
+        }
+      }
+    }
+    Vue.createApp(EventHandling).mount('#event-handling')
+});
